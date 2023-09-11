1f1be4111becd870e9899f6892b135d5d2cc998c
Cristiano Aloigi <cristianoaloigi0@gmail.com>
2023-09-11 21:09:34 +0200

    finita la pagina services

--- components/ServiceSlider.tsx
+++ components/ServiceSlider.tsx
@@ -11,7 +11,11 @@ import {
 } from "react-icons/rx";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-import { FreeMode, Pagination } from "swiper/modules";
+import { FreeMode, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/free-mode';
 
 // data
 export const serviceData = [
@@ -23,6 +27,36 @@ export const serviceData = [
   {
     icon: <CgWebsite />,
     title: "Web Development",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    icon: <RxMobile />,
+    title: "Mobile Development",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    icon: <RxMobile />,
+    title: "Mobile Development",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    icon: <RxMobile />,
+    title: "Mobile Development",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    icon: <RxMobile />,
+    title: "Mobile Development",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    icon: <RxMobile />,
+    title: "Mobile Development",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    icon: <RxMobile />,
+    title: "Mobile Development",
     description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
   },
   {
@@ -54,17 +88,18 @@ const ServiceSlider = () => {
         return (
             <SwiperSlide key={index}>
                 <div className="bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8
-                    flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer">
+                    flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg[rgba(89,6,169,0.15)]
+                    transition-all duration-300">
                     {/* icon */}
-                    <div>{item.icon}</div>
+                    <div className="text-4xl text-accent mb-4">{item.icon}</div>
                     {/* title & desc */}
-                    <div>
-                        <div>{item.title}</div>
-                        <p>{item.description}</p>
+                    <div className="mb-8">
+                        <div className="mb-2 text-lg">{item.title}</div>
+                        <p className="max-w-[350px] leading-normal">{item.description}</p>
                     </div>
                     {/* arrow */}
                     <div className="text-3xl">
-                        <RxArrowTopRight />
+                        <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-accent transition-all duration-300"/>
                     </div>
                 </div>
             </SwiperSlide>
--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -17,16 +17,21 @@ const Services = () => {
                     {/* text */}
                     <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4
                         xl:mb-0">
-                        <h2 className="h2 xl:mt-8">
+                        <motion.h2 variants={fadeIn('up', 0.2)} initial='hidden' animate="show"
+                            exit="hidden" className="h2 xl:mt-8">
                             My services <span className="text-accent">.</span>
-                        </h2>
-                        <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        </motion.h2>
+                        <motion.p variants={fadeIn('up', 0.4)} initial='hidden' animate="show"
+                            exit="hidden" className="mb-4 max-w-[400px] mx-auto lg:mx-0">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
                             Quidem enim nobis voluptatum fuga vitae commodi quo doloribus
-                        </p>
+                        </motion.p>
                     </div>
                     {/* slider */}
-                    <ServiceSlider />
+                    <motion.div variants={fadeIn('down', 0.6)} initial='hidden' animate="show"
+                        exit="hidden" className="w-full xl:max-w-[65%]">
+                        <ServiceSlider />
+                    </motion.div>
                 </div>
             </div>
             <Bulb />
